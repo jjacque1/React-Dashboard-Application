a41f5b07d1772a9b441eda3e5d54c102e5ce7fbc
jjacque1 <jacksonjacque.jj@gmail.com>
2025-12-11 15:39:36 -0500

    copied TaskList component logic from the lab and added it into TaskList.tsx

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,27 @@
+
+import React from "react";
+import type { TaskListProps } from "../../types";
+import { TaskItem } from "./TaskItem";
+
+export function TaskList({
+  tasks,
+  onStatusChange,
+  onDelete,
+}: TaskListProps) {
+  if (tasks.length === 0) {
+    return <p>No tasks match the current filters.</p>;
+  }
+
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onStatusChange={onStatusChange}
+          onDelete={onDelete}
+        />
+      ))}
+    </ul>
+  );
+}
